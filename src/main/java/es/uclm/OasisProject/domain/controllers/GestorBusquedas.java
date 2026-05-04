3ceb953d9fad4b06333211476f6d71006cf13ed1
Carlos Lopez <Carlos.lopez54@alu.uclm.es>
2026-05-04 19:38:11 +0200

    Lista de deseos de usuarios implementada #18

--- src/main/java/es/uclm/OasisProject/domain/controllers/GestorBusquedas.java
+++ src/main/java/es/uclm/OasisProject/domain/controllers/GestorBusquedas.java
@@ -2,20 +2,65 @@ package es.uclm.OasisProject.domain.controllers;
 
 import java.time.LocalDate;
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import es.uclm.OasisProject.domain.entities.Inmueble;
+import es.uclm.OasisProject.domain.entities.Inquilino;
+import es.uclm.OasisProject.domain.entities.Usuario;
 import es.uclm.OasisProject.persistence.InmuebleDAO;
+import es.uclm.OasisProject.persistence.UsuarioDAO;
 
 @Service
 public class GestorBusquedas {
 	
+	private static final Logger log = LoggerFactory.getLogger(GestorInmuebles.class);
+	
 	@Autowired
 	private InmuebleDAO inmuebleDAO;
 	
+	@Autowired
+	private UsuarioDAO usuarioDAO;
+	
 	public List<Inmueble> buscarInmuebles(LocalDate fechaInicio, LocalDate fechaFin) {
 		return inmuebleDAO.findDisponibles(fechaInicio, fechaFin);
+	}
+	
+	public boolean anadirInmueble(String login, Inmueble inmueble) {
+		
+		Usuario usuario = usuarioDAO.findByLogin(login);
+		
+		if (usuario == null) {
+	        log.warn("El usuario no existe");
+	        return false;
+	    }
+		
+		if (!(usuario instanceof Inquilino)) {
+	        log.warn("El usuario no es un inquilino");
+	        return false;
+	    }
+		
+		
+		if (inmueble == null) {
+	        log.warn("El inmueble no existe");
+	        return false;
+	    }
+		
+		Inquilino inquilino = (Inquilino) usuario;
+		
+		if (inquilino.getListaDeseos().contains(inmueble)) {
+		    log.warn("El inmueble ya está en la lista de deseos");
+		    return false;
+		}
+		
+		inquilino.addListaDeseos(inmueble);
+		usuarioDAO.update(inquilino);
+		
+		return true;
+		
+		
+		
 	}
 	
 	
--- src/main/java/es/uclm/OasisProject/presentation/VentanaBusqueda.java
+++ src/main/java/es/uclm/OasisProject/presentation/VentanaBusqueda.java
@@ -1,19 +1,26 @@
 package es.uclm.OasisProject.presentation;
 
+import java.security.Principal;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import es.uclm.OasisProject.domain.controllers.GestorBusquedas;
 import es.uclm.OasisProject.domain.entities.Inmueble;
+import es.uclm.OasisProject.persistence.InmuebleDAO;
 
 @Controller
 public class VentanaBusqueda {
 	
 	@Autowired
 	private GestorBusquedas gestorBusquedas;
+	
+	@Autowired
+    InmuebleDAO inmuebleDAO;
 	
 
 	@GetMapping("/busqueda")
@@ -29,6 +36,24 @@ public class VentanaBusqueda {
 
         model.addAttribute("resultados", resultados);
         return "ResultadosBusqueda";
+    }
+    
+    @PostMapping("/anadirFavorito")
+    public String anadirFavorito(@RequestParam int idInmueble, Principal principal, RedirectAttributes redirectAttributes) {
+
+        String login = principal.getName();
+        
+        Inmueble inmueble = inmuebleDAO.select(idInmueble);
+
+        boolean exito = gestorBusquedas.anadirInmueble(login, inmueble);
+
+        if (exito) {
+            redirectAttributes.addFlashAttribute("mensaje", "Añadido a favoritos");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "No se pudo añadir");
+        }
+
+        return "redirect:/buscarInmuebles";
     }
 
 
