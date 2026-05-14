74ab8ae27d0e207d3895257cb4c9c59b177bc59f
Carlos Lopez <Carlos.lopez54@alu.uclm.es>
2026-05-14 17:19:28 +0200

    Cierre de sesion implementado #23

--- src/main/java/es/uclm/OasisProject/config/SecurityConfig.java
+++ src/main/java/es/uclm/OasisProject/config/SecurityConfig.java
@@ -63,9 +63,9 @@ public class SecurityConfig {
                 .permitAll()
             )
             .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
-                .permitAll()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login")
+                    .permitAll()
             );
 
         return http.build();
