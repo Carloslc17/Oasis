10c991694c403b51e66e6a5e4df2c91bf4bd88e7
Carlos Lopez <Carlos.lopez54@alu.uclm.es>
2026-06-02 19:07:50 +0200

    Error en nombramiento de paquetes #38

---
+++ src/test/java/es/uclm/OasisProject/persistence/ReservaDAOTest.java
@@ -0,0 +1,73 @@
+package es.uclm.OasisProjectpersistence;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import es.uclm.OasisProject.domain.entities.Inquilino;
+import es.uclm.OasisProject.domain.entities.Reserva;
+import es.uclm.OasisProject.persistence.GestorBD;
+import es.uclm.OasisProject.persistence.ReservaDAO;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+public class ReservaDAOTest {
+	
+
+@InjectMocks
+    private ReservaDAO dao;
+
+    @Mock
+    private GestorBD gestorBD;
+
+    @Mock
+    private EntityManager entityManager;
+
+    @Mock
+    private TypedQuery<Reserva> query;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        when(gestorBD.getEntityManager()).thenReturn(entityManager);
+    }
+
+    @Test
+    void findByInquilino_ok() {
+        Inquilino inq = new Inquilino();
+
+        when(entityManager.createQuery(anyString(), eq(Reserva.class)))
+                .thenReturn(query);
+
+        when(query.setParameter("inquilino", inq)).thenReturn(query);
+        when(query.getResultList()).thenReturn(List.of(new Reserva()));
+
+        List<Reserva> result = dao.findByInquilino(inq);
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByInquilino_empty() {
+        Inquilino inq = new Inquilino();
+
+        when(entityManager.createQuery(anyString(), eq(Reserva.class)))
+                .thenReturn(query);
+
+        when(query.setParameter("inquilino", inq)).thenReturn(query);
+        when(query.getResultList()).thenReturn(List.of());
+
+        List<Reserva> result = dao.findByInquilino(inq);
+
+        assertTrue(result.isEmpty());
+    }
+
+
+}
